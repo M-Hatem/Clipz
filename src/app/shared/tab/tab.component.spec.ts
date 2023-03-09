b103929ae02c816f520af8cfd3a43c1dc81ef60a
MadoPlus <ma7modmado@gmail.com>
2023-03-09 07:50:33 +0200

    Add Tab component spec file logic

--- src/app/shared/tab/tab.component.spec.ts
+++ src/app/shared/tab/tab.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { TabComponent } from './tab.component';
 
 describe('TabComponent', () => {
@@ -8,9 +8,8 @@ describe('TabComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TabComponent ]
-    })
-    .compileComponents();
+      declarations: [TabComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TabComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,18 @@ describe('TabComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have .hidden class', () => {
+    const element = fixture.debugElement.query(By.css('.hidden'));
+    expect(element).toBeTruthy();
+  });
+
+  it('should not have .hidden class', () => {
+    component.active = true;
+    fixture.detectChanges();
+
+    const element = fixture.debugElement.query(By.css('.hidden'));
+    expect(element).not.toBeTruthy();
   });
 });
