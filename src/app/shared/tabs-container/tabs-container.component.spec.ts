6fc435bda5db383ba594ff832a06dda50696ce54
MadoPlus <ma7modmado@gmail.com>
2023-03-09 07:50:57 +0200

    Add Tabs-Container component spec file logic

--- src/app/shared/tabs-container/tabs-container.component.spec.ts
+++ src/app/shared/tabs-container/tabs-container.component.spec.ts
@@ -1,23 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 import { TabsContainerComponent } from './tabs-container.component';
+import { TabComponent } from '../tab/tab.component';
+@Component({
+  template: `<app-tabs-container>
+    <app-tab tabTitle="Tab 1">Tab 1</app-tab>
+    <app-tab tabTitle="Tab 2">Tab 2</app-tab>
+  </app-tabs-container>`,
+})
+class TestHostComponent {}
 
 describe('TabsContainerComponent', () => {
-  let component: TabsContainerComponent;
-  let fixture: ComponentFixture<TabsContainerComponent>;
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TabsContainerComponent ]
-    })
-    .compileComponents();
+      declarations: [TabsContainerComponent, TestHostComponent, TabComponent],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(TabsContainerComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have two tabs', () => {
+    const tabs = fixture.debugElement.queryAll(By.css('li'));
+    const containerComponent = fixture.debugElement.query(
+      By.directive(TabsContainerComponent)
+    );
+    const tabsProp = containerComponent.componentInstance.tabs;
+
+    expect(tabs.length).withContext("Tabs didn't render").toBe(2);
+    expect(tabsProp.length)
+      .withContext("Couldn't grab component property")
+      .toBe(2);
   });
 });
